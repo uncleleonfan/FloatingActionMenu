c813a6d2db7668a2b4bcd0dc2aaff660497da591
uncleleonfan <526247082@qq.com>
2016-05-03 09:53:01 +0800

    Using FloatingActionMenu.

---
+++ app/src/main/java/com/leon/floatingactionmenu/behavior/ScrollBehavior.java
@@ -0,0 +1,16 @@
+package com.leon.floatingactionmenu.behavior;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.leon.floatingactionmenu.widget.FloatingActionMenu;
+
+/**
+ * Created by Leon on 2016/4/8.
+ */
+public class ScrollBehavior extends FloatingActionMenu.Behavior {
+
+    public ScrollBehavior(Context context, AttributeSet attributeSet) {
+        super();
+    }
+}
